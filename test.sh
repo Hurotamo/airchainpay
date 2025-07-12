1fa987a8bbf0f84ec7776869be7e778f6579d68d
User Name <user@example.com>
2025-07-12 13:43:08 +0800

    chore: commit all staged and unstaged changes across the repo

--- test.sh
+++ test.sh
@@ -9,3 +9,14 @@
 
 wallet 
  npm install --save-dev dotenv-cli
+
+
+Remove other sensitive files from tracking:
+
+    git rm --cached airchainpay-relay/server.cert
+   git rm --cached airchainpay-relay/.env.dev
+   git rm --cached airchainpay-relay/.env.prod
+
+Immediately remove the private key from git history:
+
+      git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch airchainpay-contracts/.env' --prune-empty --tag-name-filter cat -- --all
